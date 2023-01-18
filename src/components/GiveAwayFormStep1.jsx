828c6b3a21f8185e47696be848e4dda57516cee7
Berta-Anna-Witowska <bertaannawitowska@gmail.com>
2023-01-18 23:02:19 +0100

    oddaj rzeczy -poprawki

--- src/components/GiveAwayFormStep1.jsx
+++ src/components/GiveAwayFormStep1.jsx
@@ -6,17 +6,17 @@ export default function GiveAwayFormStep1() {
       <h2>Krok 1/4</h2>
       <h1>Zaznacz co chcesz oddać:</h1>
       <div className="step1-checkboxes">
-        <input type="checkbox" id="reuseClothes" name="" autoFocus />
+        <input type="radio" id="reuseClothes" name="" autoFocus />
         <label htmlFor="reuseClothes">
           ubrania, które nadają się do ponownego użycia
         </label>
-        <input type="checkbox" id="throwawayClothes" name="" />
+        <input type="radio" id="throwawayClothes" name="" />
         <label htmlFor="throwawayClothes">ubrania, do wyrzucenia</label>
-        <input type="checkbox" id="toys" name="" />
+        <input type="radio" id="toys" name="" />
         <label htmlFor="toys">zabawki</label>
-        <input type="checkbox" id="books" name="" />
+        <input type="radio" id="books" name="" />
         <label htmlFor="books">książki</label>
-        <input type="checkbox" id="other" name="" />
+        <input type="radio" id="other" name="" />
         <label htmlFor="other">inne</label>
       </div>
     </div>
--- src/components/GiveAwayFormSummary.jsx
+++ src/components/GiveAwayFormSummary.jsx
@@ -4,10 +4,20 @@ export default function GiveAwayFormSummary() {
   return (
     <div className="form-summary">
       <h1>Podsumowanie Twojej darowizny</h1>
-      <div>Oddajesz:</div>
+      <div className="summary-thingstogive">
+        <h2>Oddajesz:</h2>
+        <div>rzeczy do oddania</div>
+        <div>lokalizacja</div>
+      </div>
       <div className="summary-box">
-        <div className="summary-adress">Adres odbioru:</div>
-        <div className="summary-date">Termin odbioru:</div>
+        <div className="summary-adress">
+          <h2>Adres odbioru:</h2>
+          <div className="adress-data">dane adres</div>
+        </div>
+        <div className="summary-date">
+          <h2>Termin odbioru:</h2>
+          <div className="date-data">dane termin</div>
+        </div>
       </div>
     </div>
   );
